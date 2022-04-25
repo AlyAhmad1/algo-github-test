526785e82b86ba75c18fa8e2db529d8bdd0bf666
“aly” <aliahmad538404@gmail.com>
2022-04-25 13:18:24 +0500

    added example for parameterize test

--- test_file.py
+++ test_file.py
@@ -1,4 +1,5 @@
 from main import *
+import pytest
 
 
 def test_parse_commands():
@@ -20,3 +21,27 @@ def test_random_length():
 
 def test_bad_length():
     assert len(bad_co()) == 3
+
+
+# parametrize function example see below------------------------
+
+# pytest fixtures
+@pytest.fixture()
+def file_data():
+    with open('data.txt', 'r') as file:
+        data = json.loads(file.read())
+    return data
+
+
+def parse_co(file_data):
+    parse_c = []
+    for row in file_data:
+        if 'function' in row and row['function'] == 'parse':
+            parse_c.append(row.copy())
+    return parse_c
+
+
+# parameterize test cases
+@pytest.mark.parametrize("compare_with", [{'function': 'parse', 'help': 'file help', 'value': 'file'}])
+def test_parse_command_test(file_data, compare_with):
+    assert parse_co(file_data) == [compare_with]
